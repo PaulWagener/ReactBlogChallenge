9951bbf28bf09d0ae366d3b37c9930e68b4d412a
Paul Wagener <mail@paulwagener.nl>
2017-09-14 15:24:45 +0200

    Fix warnings in Menu

--- src/blog/Menu.tsx
+++ src/blog/Menu.tsx
@@ -1,26 +1,25 @@
 import * as React from 'react';
 import Tabs, { Tab } from 'material-ui/Tabs';
-import { withRouter } from 'react-router-dom';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
-class Menu extends React.Component {
-  props: {
-    history?: any
-  };
+const MenuRouter = withRouter<{}>(
+  class Menu extends React.Component<RouteComponentProps<{}>, {}> {
 
-  handleChange = (event: object, value: number) => {
-    this.props.history.push(value);
+    handleChange = (event: object, value: string) => {
+      this.props.history.push(value);
+    }
+
+    render() {
+      return (
+        <nav>
+          <Tabs value={this.props.history.location.pathname} onChange={this.handleChange} centered={true}>
+            <Tab value="/" label="List" />
+            <Tab value="/add" label="Add new blog" />
+          </Tabs>
+        </nav>
+      );
+    }
   }
+);
 
-  render() {
-    return (
-      <nav>
-        <Tabs value={this.props.history.location.pathname} onChange={this.handleChange} centered={true}>
-          <Tab value="/" label="List" />
-          <Tab value="/add" label="Add new blog" />
-        </Tabs>
-      </nav>
-    );
-  }
-}
-
-export default withRouter(Menu);
+export default MenuRouter;
